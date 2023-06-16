2b92cf7436991f839ef9b30b516eca2bdc72ba97
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-06-16 17:47:05 +0200

    Revert password mismatch back to my original solution

--- script.js
+++ script.js
@@ -2,26 +2,21 @@
 
 const newPassword = document.querySelector("#new-password");
 const confirmPassword = document.querySelector("#confirm-password");
-const passwordWarning = document.querySelector(".password-warning");
 
 confirmPassword.addEventListener("input", () => {
         if (confirmPassword.value !== newPassword.value) {
             confirmPassword.setCustomValidity("Passwords do not match");
-            passwordWarning.style.visibility = "visible";
         } else if (confirmPassword.value === newPassword.value) {
             confirmPassword.setCustomValidity("");
-            passwordWarning.style.visibility = "hidden";
         }
 });
 
 newPassword.addEventListener("input", () => {
-    if (confirmPassword !== "") {
+    if (confirmPassword.value !== "") {
     if (confirmPassword.value !== newPassword.value) {
         confirmPassword.setCustomValidity("Passwords do not match");
-        passwordWarning.style.visibility = "visible";
     } else if (confirmPassword.value === newPassword.value) {
         confirmPassword.setCustomValidity("");
-        passwordWarning.style.visibility = "hidden";
     }
     }
 });
